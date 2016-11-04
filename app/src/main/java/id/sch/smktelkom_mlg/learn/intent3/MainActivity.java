8fd5515547a5044d1973d978b5a83ce60ad8c6ae
yogasulton <yogamuslim21@gmail.com>
2016-11-04 13:32:59 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -20,12 +20,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang ");
+            }
+        });
+
         findViewById(R.id.imageViewCamera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 capturePhoto();
             }
         });
+    }
+
+    private void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void dialPhoneNumber(String phoneNumber) {
